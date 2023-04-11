88ab737d203a98b4785fe1b07073e2df800efa0e
msouvanna <msouvanna@ualr.edu>
2023-04-11 18:43:30 +0000

    messed with charts

--- test/assets/scripts/index.js
+++ test/assets/scripts/index.js
@@ -1,13 +1,45 @@
-async function displayHeight(){
+async function displayHeight() {
     var myURL = "https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=07055646&period=P7D&parameterCd=00065&siteStatus=all";
 
-    /* fetch the results */
-    var heightObject = await fetch(myURL);
-    if (heightObject.status>=200 && heightObject.status<=299){
-        var heightObjectJSONText = await heightObject.text();
+    var msgObject = await fetch(myURL);
+    var msgJSONText = await msgObject.text();
+    var msg = JSON.parse(msgJSONText);
+   // document.getElementById("test").innerHTML = msgJSONText;
 
-        var heightmsg = JSON.parse(heightObjectJSONText);
+    /* Site 1 */
+var dates = [];
+var values = [];
+/* fLen contains the length of the array (number of values) */
+fLen = msg.value.timeSeries[0].values[0].value.length;
+for (i = 0; i < fLen; i++) {
+    values[i] = msg.value.timeSeries[0].values[0].value[i].value;
+	dates[i] = msg.value.timeSeries[0].values[0].value[i].dateTime;
+}
+var sitename = msg.value.timeSeries[0].sourceInfo.siteName;
+var sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value;
+var siteDescription = msg.value.timeSeries[0].variable.variableDescription;
 
-        document.getElementById("test").innerHTML = heightObjectJSONText;
-    }
+
+
+/* Put your code here to display a graph of values and dates for Site 1*/
+var ctx0 = document.getElementById("chartjs-0");
+
+var myChart = new Chart(ctx0, {
+    "type": "line",
+    "data": {
+        "labels": dates, 
+        "datasets":[{
+            "label": "Site 1 River Height",
+            "data": values,
+            "fill": false,
+            "borderColor":"rgb(75, 192, 192)",
+            "lineTension":0.1
+        }]},
+        "options":{
+        "responsive": false,
+        "maintainAspectRatio":true,
+        }
+});
+
+
 }
