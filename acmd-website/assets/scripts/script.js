9ad72f612c89ce55c91ad9f9bfb9c59f719783f6
msouvanna <msouvanna@ualr.edu>
2023-04-18 17:59:22 +0000

    april 18 - saving workspace to go do lecture

---
+++ acmd-website/assets/scripts/script.js
@@ -0,0 +1,21 @@
+// THIS IS FOR THE IMAGE CARSOUSEL ON THE HOME PAGE //
+
+/* Alternate Configuation Options Here - https://accessible360.github.io/accessible-slick/#demos */
+
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+});
+
+// ---------- cont. below ----- //
