a5a5e59fe6f64c83b3205e61e91dc98e0cd819cc
msouvanna <msouvanna@ualr.edu>
2023-04-20 19:56:24 +0000

    4/20 save after class

--- test/assets/scripts/index.js
+++ test/assets/scripts/index.js
@@ -4,178 +4,193 @@ async function displayHeight() {
     var msgObject = await fetch(myURL);
     var msgJSONText = await msgObject.text();
     var msg = JSON.parse(msgJSONText);
-   // document.getElementById("test").innerHTML = msgJSONText;
+    // document.getElementById("test").innerHTML = msgJSONText;
 
     /* Site 1 */
-var dates = [];
-var values = [];
-/* fLen contains the length of the array (number of values) */
-fLen = msg.value.timeSeries[0].values[0].value.length;
-for (i = 0; i < fLen; i++) {
-    values[i] = msg.value.timeSeries[0].values[0].value[i].value;
-	dates[i] = msg.value.timeSeries[0].values[0].value[i].dateTime;
-}
-var sitename = msg.value.timeSeries[0].sourceInfo.siteName;
-var sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value;
-var siteDescription = msg.value.timeSeries[0].variable.variableDescription;
+    var dates = [];
+    var values = [];
+    /* fLen contains the length of the array (number of values) */
+    fLen = msg.value.timeSeries[0].values[0].value.length;
+    for (i = 0; i < fLen; i++) {
+        values[i] = msg.value.timeSeries[0].values[0].value[i].value;
+        dates[i] = msg.value.timeSeries[0].values[0].value[i].dateTime;
+    }
+    var sitename = msg.value.timeSeries[0].sourceInfo.siteName;
+    var sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value;
+    var siteDescription = msg.value.timeSeries[0].variable.variableDescription;
 
 
 
-/* Put your code here to display a graph of values and dates for Site 1*/
-var ctx0 = document.getElementById("chartjs-0");
+    /* Put your code here to display a graph of values and dates for Site 1*/
+    var ctx0 = document.getElementById("chartjs-0");
 
-var myChart = new Chart(ctx0, {
-    "type": "line",
-    "data": {
-        "labels": dates, 
-        "datasets":[{
-            "label": sitename,
-            "data": values,
-            "fill": false,
-            "borderColor":"rgb(75, 192, 192)",
-            "lineTension":0.1
-        }]},
-        "options":{
-        "responsive": false,
-        "maintainAspectRatio":true,
+    var myChart = new Chart(ctx0, {
+        "type": "line",
+        "data": {
+            "labels": dates,
+            "datasets": [{
+                label: sitename,
+                data: values,
+                fill: false,
+                borderColor: "green",
+                lineTension: 0.1
+            }]
+        },
+        "options": {
+            responsive: false,
+            maintainAspectRatio: true,
+
         }
-});
+    });
 
-/* Site 2 */
-var dates = [];
-var valuess = [];
-/* fLen contains the length of the array (number of values) */
-fLen = msg.value.timeSeries[1].values[0].value.length
-for (i = 0; i < fLen; i++) {
-    valuess[i] = msg.value.timeSeries[1].values[0].value[i].value
-	dates[i] = msg.value.timeSeries[1].values[0].value[i].dateTime
-}
-var sitenamee = msg.value.timeSeries[1].sourceInfo.siteName
-var sitecode = msg.value.timeSeries[1].sourceInfo.siteCode[0].value
-var siteDescription = msg.value.timeSeries[1].variable.variableDescription
+    /* Site 2 */
+    var dates = [];
+    var valuess = [];
+    /* fLen contains the length of the array (number of values) */
+    fLen = msg.value.timeSeries[1].values[0].value.length
+    for (i = 0; i < fLen; i++) {
+        valuess[i] = msg.value.timeSeries[1].values[0].value[i].value
+        dates[i] = msg.value.timeSeries[1].values[0].value[i].dateTime
+    }
+    var sitenamee = msg.value.timeSeries[1].sourceInfo.siteName
+    var sitecode = msg.value.timeSeries[1].sourceInfo.siteCode[0].value
+    var siteDescription = msg.value.timeSeries[1].variable.variableDescription
 
-var ctx1 = document.getElementById("chartjs-1");
+    var ctx1 = document.getElementById("chartjs-1");
 
-var myChart = new Chart(ctx1, {
-    "type": "line",
-    "data": {
-        "labels": dates, 
-        "datasets":[{
-            "label": sitenamee,
-            "data": valuess,
-            "fill": false,
-            "borderColor":"rgb(75, 192, 192)",
-            "lineTension":0.1
-        }]},
-        "options":{
-        "responsive": false,
-        "maintainAspectRatio":true,
+    var myChart = new Chart(ctx1, {
+        "type": "line",
+        "data": {
+            "labels": dates,
+            "datasets": [{
+                "label": sitenamee,
+                "data": valuess,
+                "fill": false,
+                "borderColor": "rgb(75, 192, 192)",
+                "lineTension": 0.1
+            }]
+        },
+
+        "options": {
+            "responsive": false,
+            "maintainAspectRatio": true,
         }
-});
+    });
 
-/* Site 3 */
-var dates = [];
-var valuesss = [];
-/* fLen contains the length of the array (number of values) */
-fLen = msg.value.timeSeries[2].values[0].value.length
-for (i = 0; i < fLen; i++) {
-    valuesss[i] = msg.value.timeSeries[2].values[0].value[i].value
-	dates[i] = msg.value.timeSeries[2].values[0].value[i].dateTime
-}
-var sitenameee = msg.value.timeSeries[2].sourceInfo.siteName
-var sitecode = msg.value.timeSeries[2].sourceInfo.siteCode[0].value
-var siteDescription = msg.value.timeSeries[2].variable.variableDescription
+    /* Site 3 */
+    var dates = [];
+    var valuesss = [];
+    /* fLen contains the length of the array (number of values) */
+    fLen = msg.value.timeSeries[2].values[0].value.length
+    for (i = 0; i < fLen; i++) {
+        valuesss[i] = msg.value.timeSeries[2].values[0].value[i].value
+        dates[i] = msg.value.timeSeries[2].values[0].value[i].dateTime
+    }
+    var sitenameee = msg.value.timeSeries[2].sourceInfo.siteName
+    var sitecode = msg.value.timeSeries[2].sourceInfo.siteCode[0].value
+    var siteDescription = msg.value.timeSeries[2].variable.variableDescription
 
-var ctx2 = document.getElementById("chartjs-2");
+    var ctx2 = document.getElementById("chartjs-2");
 
-var myChart = new Chart(ctx2, {
-    "type": "line",
-    "data": {
-        "labels": dates, 
-        "datasets":[{
-            "label": sitenameee,
-            "data": valuesss,
-            "fill": false,
-            "borderColor":"rgb(75, 192, 192)",
-            "lineTension":0.1
-        }]},
-        "options":{
-        "responsive": false,
-        "maintainAspectRatio":true,
+    var myChart = new Chart(ctx2, {
+        "type": "line",
+        "data": {
+            "labels": dates,
+            "datasets": [{
+                "label": sitenameee,
+                "data": valuesss,
+                "fill": false,
+                "borderColor": "rgb(75, 192, 192)",
+                "lineTension": 0.1
+            }]
+        },
+        "options": {
+            "responsive": false,
+            "maintainAspectRatio": true,
         }
-});
+    });
 
-/* Site 4 */
-var dates = [];
-var valuessss = [];
-/* fLen contains the length of the array (number of values) */
-fLen = msg.value.timeSeries[3].values[0].value.length
-for (i = 0; i < fLen; i++) {
-    valuessss[i] = msg.value.timeSeries[3].values[0].value[i].value
-	dates[i] = msg.value.timeSeries[3].values[0].value[i].dateTime
-}
-var sitenameeee = msg.value.timeSeries[3].sourceInfo.siteName
-var sitecode = msg.value.timeSeries[3].sourceInfo.siteCode[0].value
-var siteDescription = msg.value.timeSeries[3].variable.variableDescription
+    /* Site 4 */
+    var dates = [];
+    var valuessss = [];
+    /* fLen contains the length of the array (number of values) */
+    fLen = msg.value.timeSeries[3].values[0].value.length
+    for (i = 0; i < fLen; i++) {
+        valuessss[i] = msg.value.timeSeries[3].values[0].value[i].value
+        dates[i] = msg.value.timeSeries[3].values[0].value[i].dateTime
+    }
+    var sitenameeee = msg.value.timeSeries[3].sourceInfo.siteName
+    var sitecode = msg.value.timeSeries[3].sourceInfo.siteCode[0].value
+    var siteDescription = msg.value.timeSeries[3].variable.variableDescription
 
-var ctx3 = document.getElementById("chartjs-3");
+    var ctx3 = document.getElementById("chartjs-3");
 
-var myChart = new Chart(ctx3, {
-    "type": "line",
-    "data": {
-        "labels": dates, 
-        "datasets":[{
-            "label": sitenameeee,
-            "data": valuessss,
-            "fill": false,
-            "borderColor":"rgb(75, 192, 192)",
-            "lineTension":0.1
-        }]},
-        "options":{
-        "responsive": false,
-        "maintainAspectRatio":true,
+    var myChart = new Chart(ctx3, {
+        "type": "line",
+        "data": {
+            "labels": dates,
+            "datasets": [{
+                "label": sitenameeee,
+                "data": valuessss,
+                "fill": false,
+                "borderColor": "rgb(75, 192, 192)",
+                "lineTension": 0.1
+            }]
+        },
+        "options": {
+            "responsive": false,
+            "maintainAspectRatio": true,
         }
-});
+    });
 
-var ctx4 = document.getElementById("chartjs-4");
+    var ctx4 = document.getElementById("chartjs-4");
 
-var myChart = new Chart(ctx4, {
-    "type": "line",
-    "data": {
-        "labels": dates, 
-        "datasets":[{
-            "label": sitename, //site 1
-            "data": values,
-            "fill": false,
-            "borderColor":"red",
-            "lineTension":0.1
-        },{
-            "label":sitenamee, //site 2
-            "data":valuess,
-            "fill":false,
-            "borderColor":"blue",
-            "lineTension":0.1
-        },{
-            "label":sitenameee, //site 3
-            "data":valuesss,
-            "fill":false,
-            "borderColor":"green",
-            "lineTension":0.1
-        }, {
-            "label":sitenameeee, //site 4
-            "data":valuessss,
-            "fill":false,
-            "borderColor":"purple",
-            "lineTension":0.1
-        }]},
-
-        "options":{
-        "responsive": false,
-        "maintainAspectRatio":true,
+    var myChart = new Chart(ctx4, {
+        "type": "line",
+        "data": {
+            "labels": dates,
+            "datasets": [{
+                "label": sitename, //site 1
+                "data": values,
+                "fill": false,
+                "borderColor": "red",
+                "lineTension": 0.1
+            }, {
+                "label": sitenamee, //site 2
+                "data": valuess,
+                "fill": false,
+                "borderColor": "blue",
+                "lineTension": 0.1
+            }, {
+                "label": sitenameee, //site 3
+                "data": valuesss,
+                "fill": false,
+                "borderColor": "green",
+                "lineTension": 0.1
+            }, {
+                "label": sitenameeee, //site 4
+                "data": valuessss,
+                "fill": false,
+                "borderColor": "yellow",
+                "lineTension": 0.1
+            }]
+        },
+        "options": {
+            "responsive": false,
+            "maintainAspectRatio": true,
+            "title":{
+                display:true,
+                text:"hello, can anyone see this?"
+            },
+            "scales":{
+                "x":{
+                    
+                }
+            }
         }
-});
+    });
 
-      
+
 
 
 
